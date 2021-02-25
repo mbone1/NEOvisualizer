24a7c18efcb4fb0470b2e8a4cc6a0c6687d28490
mbone1 <m.bonnev10@gmail.com>
2021-02-25 16:57:18 -0700

    success with front/back connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState, } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [data, setData] = useState({});
+  
+  function fetchData() {
+    const result = fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => setData({ apiResponse: res }))
+      .catch(err => err);
+
+  }
+
+  
+
+  useEffect(() => {
+    // const result = await axios("http://localhost:9000/testAPI");
+    fetchData()
+    
+    // async function fetchData() {
+    //   const result = axios("http://localhost:9000/testAPI");
+    //   setData (result)
+    // }
+    // fetchData()
+  }, []);
+
+
+  // callAPI() {
+  //   fetch("http://localhost:9000/testAPI")
+  //     .then(res => res.text())
+  //     .then(res => this.setState({ apiResponse: res }));
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* {data} */}
+        {/* {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+        </li>
+      ))} */}
+        {data.apiResponse}
       </header>
     </div>
   );
